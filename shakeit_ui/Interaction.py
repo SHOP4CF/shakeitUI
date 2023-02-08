0149aa8114096739e8b808e32ad8e452ed514956
Jeshith <jeda@teknologisk.dk>
2023-02-08 16:53:24 +0100

    Working with 30 btns through cmd

--- shakeit_ui/Interaction.py
+++ shakeit_ui/Interaction.py
@@ -18,17 +18,54 @@ class InteractionWindow:
         # connecting buttons
         self.ui.buttonStart.clicked.connect(self.start)
         self.ui.buttonReady.clicked.connect(self.ready)
-        self.ui.buttonEnd.clicked.connect(self.end)
+        #self.ui.buttonEnd.clicked.connect(self.end)
 
         self.ui.buttonExit.clicked.connect(self.done)
         self.ui.buttonExit_2.clicked.connect(self.exit)
         self.ui.buttonExit_3.clicked.connect(self.exit)
         self.ui.buttonExit_4.clicked.connect(self.exit)
 
-        # connecting buttons for anyfeeder
+        # connecting training buttons for anyfeeder
         self.ui.buttonforward.clicked.connect(self.trainforward)
         self.ui.buttonFlip.clicked.connect(self.trainflip)
         self.ui.buttonback.clicked.connect(self.trainbackward)
+
+        # connecting buttons for anyfeeder
+        # btn shake forward
+        self.ui.pushButton_52.clicked.connect(self.fw_s2_r1)
+        self.ui.pushButton_45.clicked.connect(self.fw_s4_r1)
+        self.ui.pushButton_48.clicked.connect(self.fw_s6_r1)
+        self.ui.pushButton_43.clicked.connect(self.fw_s8_r1)
+        self.ui.pushButton_47.clicked.connect(self.fw_s10_r1)
+        self.ui.pushButton_46.clicked.connect(self.fw_s2_r3)
+        self.ui.pushButton_44.clicked.connect(self.fw_s4_r3)
+        self.ui.pushButton_50.clicked.connect(self.fw_s6_r3)
+        self.ui.pushButton_51.clicked.connect(self.fw_s8_r3)
+        self.ui.pushButton_49.clicked.connect(self.fw_s10_r3)
+
+        # btn shake flip
+        self.ui.buttonEnd.clicked.connect(self.flip_s2_r1)
+        self.ui.pushButton_35.clicked.connect(self.flip_s4_r1)
+        self.ui.pushButton_38.clicked.connect(self.flip_s6_r1)
+        self.ui.pushButton_33.clicked.connect(self.flip_s8_r1)
+        self.ui.pushButton_37.clicked.connect(self.flip_s10_r1)
+        self.ui.pushButton_36.clicked.connect(self.flip_s2_r3)
+        self.ui.pushButton_34.clicked.connect(self.flip_s4_r3)
+        self.ui.pushButton_40.clicked.connect(self.flip_s6_r3)
+        self.ui.pushButton_41.clicked.connect(self.flip_s8_r3)
+        self.ui.pushButton_39.clicked.connect(self.flip_s10_r3)
+
+        # btn shake backward
+        self.ui.pushButton_32.clicked.connect(self.bw_s2_r1)
+        self.ui.pushButton_25.clicked.connect(self.bw_s4_r1)
+        self.ui.pushButton_28.clicked.connect(self.bw_s6_r1)
+        self.ui.pushButton_23.clicked.connect(self.bw_s8_r1)
+        self.ui.pushButton_27.clicked.connect(self.bw_s10_r1)
+        self.ui.pushButton_26.clicked.connect(self.bw_s2_r3)
+        self.ui.pushButton_24.clicked.connect(self.bw_s4_r3)
+        self.ui.pushButton_30.clicked.connect(self.bw_s6_r3)
+        self.ui.pushButton_31.clicked.connect(self.bw_s8_r3)
+        self.ui.pushButton_29.clicked.connect(self.bw_s10_r3)
 
     def getWidget(self):
         return self.interaction
@@ -70,3 +107,141 @@ class InteractionWindow:
         #ros2 service call /anyfeeder_node/init anyfeeder_interfaces/srv/StandardInput "{parameters: {repetitions: 0, speed: 0}}"
         fb_3_8 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_backward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 2, speed: 6}}"""], stdout=subprocess.PIPE)
         output = fb_3_8.communicate()[0]
+
+    # Tried the following, which did not work, look into it later
+    # def fw_s_r(self, speeds, repitition):
+    #     value = f"{{parameters: {{repetitions: {repitition}, speed: {speeds}}}}}"
+    #     fw_s2_s1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_forward", "anyfeeder_interfaces/srv/StandardInput", value], stdout=subprocess.PIPE)
+    #     output = fw_s2_s1.communicate()[0]
+    
+    #
+    # FORWARD CMDs
+    #
+
+    def fw_s2_r1(self):
+        fw_2_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_forward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 1, speed: 2}}"""], stdout=subprocess.PIPE)
+        output = fw_2_1.communicate()[0]
+    
+    def fw_s4_r1(self):
+        fw_4_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_forward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 1, speed: 4}}"""], stdout=subprocess.PIPE)
+        output = fw_4_1.communicate()[0]
+
+    def fw_s6_r1(self):
+        fw_6_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_forward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 1, speed: 6}}"""], stdout=subprocess.PIPE)
+        output = fw_6_1.communicate()[0]
+
+    def fw_s8_r1(self):
+        fw_8_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_forward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 1, speed: 8}}"""], stdout=subprocess.PIPE)
+        output = fw_8_1.communicate()[0]
+
+    def fw_s10_r1(self):
+        fw_10_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_forward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 1, speed: 10}}"""], stdout=subprocess.PIPE)
+        output = fw_10_1.communicate()[0]
+
+    def fw_s2_r3(self):
+        fw_2_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_forward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 3, speed: 2}}"""], stdout=subprocess.PIPE)
+        output = fw_2_1.communicate()[0]
+    
+    def fw_s4_r3(self):
+        fw_4_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_forward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 3, speed: 4}}"""], stdout=subprocess.PIPE)
+        output = fw_4_1.communicate()[0]
+
+    def fw_s6_r3(self):
+        fw_6_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_forward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 3, speed: 6}}"""], stdout=subprocess.PIPE)
+        output = fw_6_1.communicate()[0]
+
+    def fw_s8_r3(self):
+        fw_8_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_forward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 3, speed: 8}}"""], stdout=subprocess.PIPE)
+        output = fw_8_1.communicate()[0]
+
+    def fw_s10_r3(self):
+        fw_10_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_forward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 3, speed: 10}}"""], stdout=subprocess.PIPE)
+        output = fw_10_1.communicate()[0]
+
+    #
+    # FLIP CMDs
+    #     
+
+    def flip_s2_r1(self):
+        flip_2_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/flip", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 1, speed: 2}}"""], stdout=subprocess.PIPE)
+        output = flip_2_1.communicate()[0]
+    
+    def flip_s4_r1(self):
+        flip_4_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/flip", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 1, speed: 4}}"""], stdout=subprocess.PIPE)
+        output = flip_4_1.communicate()[0]
+
+    def flip_s6_r1(self):
+        flip_6_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/flip", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 1, speed: 6}}"""], stdout=subprocess.PIPE)
+        output = flip_6_1.communicate()[0]
+
+    def flip_s8_r1(self):
+        flip_8_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/flip", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 1, speed: 8}}"""], stdout=subprocess.PIPE)
+        output = flip_8_1.communicate()[0]
+
+    def flip_s10_r1(self):
+        flip_10_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/flip", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 1, speed: 10}}"""], stdout=subprocess.PIPE)
+        output = flip_10_1.communicate()[0]
+
+    def flip_s2_r3(self):
+        flip_2_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/flip", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 3, speed: 2}}"""], stdout=subprocess.PIPE)
+        output = flip_2_1.communicate()[0]
+    
+    def flip_s4_r3(self):
+        flip_4_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/flip", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 3, speed: 4}}"""], stdout=subprocess.PIPE)
+        output = flip_4_1.communicate()[0]
+
+    def flip_s6_r3(self):
+        flip_6_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/flip", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 3, speed: 6}}"""], stdout=subprocess.PIPE)
+        output = flip_6_1.communicate()[0]
+
+    def flip_s8_r3(self):
+        flip_8_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/flip", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 3, speed: 8}}"""], stdout=subprocess.PIPE)
+        output = flip_8_1.communicate()[0]
+
+    def flip_s10_r3(self):
+        flip_10_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/flip", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 3, speed: 10}}"""], stdout=subprocess.PIPE)
+        output = flip_10_1.communicate()[0]
+
+    #
+    # BACWARDS CMDs
+    #     
+
+    def bw_s2_r1(self):
+        bw_2_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_backward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 1, speed: 2}}"""], stdout=subprocess.PIPE)
+        output = bw_2_1.communicate()[0]
+    
+    def bw_s4_r1(self):
+        bw_4_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_backward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 1, speed: 4}}"""], stdout=subprocess.PIPE)
+        output = bw_4_1.communicate()[0]
+
+    def bw_s6_r1(self):
+        bw_6_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_backward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 1, speed: 6}}"""], stdout=subprocess.PIPE)
+        output = bw_6_1.communicate()[0]
+
+    def bw_s8_r1(self):
+        bw_8_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_backward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 1, speed: 8}}"""], stdout=subprocess.PIPE)
+        output = bw_8_1.communicate()[0]
+
+    def bw_s10_r1(self):
+        bw_10_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_backward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 1, speed: 10}}"""], stdout=subprocess.PIPE)
+        output = bw_10_1.communicate()[0]
+
+    def bw_s2_r3(self):
+        bw_2_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_backward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 3, speed: 2}}"""], stdout=subprocess.PIPE)
+        output = bw_2_1.communicate()[0]
+    
+    def bw_s4_r3(self):
+        bw_4_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_backward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 3, speed: 4}}"""], stdout=subprocess.PIPE)
+        output = bw_4_1.communicate()[0]
+
+    def bw_s6_r3(self):
+        bw_6_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_backward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 3, speed: 6}}"""], stdout=subprocess.PIPE)
+        output = bw_6_1.communicate()[0]
+
+    def bw_s8_r3(self):
+        bw_8_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_backward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 3, speed: 8}}"""], stdout=subprocess.PIPE)
+        output = bw_8_1.communicate()[0]
+
+    def bw_s10_r3(self):
+        bw_10_1 = subprocess.Popen(["ros2","service","call","/anyfeeder_node/feed_backward", "anyfeeder_interfaces/srv/StandardInput", """{parameters: {repetitions: 3, speed: 10}}"""], stdout=subprocess.PIPE)
+        output = bw_10_1.communicate()[0]
